d0a6e5f791f6c7e7a6ead7b3f817cb4aec365306
Alexander Erbiste <alexander.erbite@gmail.com>
2025-04-25 22:20:29 -0300

    atualização do código

--- select.php
+++ select.php
@@ -4,8 +4,11 @@ include('database.php');
 $sql = "Select * from tb_usuario";
 
  $stmt = $con->prepare($sql);
-  $resultado = $stmt->execute();
-  $resultado->fetchAll();
+  $stmt->execute();
+  $linha = $stmt->fetch(PDO::FETCH_ASSOC);{
 
+echo ($linha)["ID_US"] . "&nbsp&nbsp" .$linha["NOME_US"] . "" . $linha["EMAIL_US"] ."<BR>" ;
+
+}
 
 ?>
